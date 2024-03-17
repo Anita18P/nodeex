1f1592d450a4460ba33872e60a177062a4d78883
Anita18P <160405870+Anita18P@users.noreply.github.com>
2024-03-17 15:25:00 +0530

    addprouct and parsing it using body parser

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const http=require('http');
 const express=require('express');
+const bodyParser = require('body-parser');
 const app=express();
-app.use((req,res,next)=>{
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.use('/add-product',(req,res,next)=>{
       console.log('In a Middleware');
-      next();
+      res.send('<form action="/product" method="POST"><input type="text" name="title"><input type="text" name="size"><button type="submit">Add Product</button></form>')
+      
 })
-app.use((req,res,next)=>{
-    console.log('In a Middleware2');
-    res.send( {key1:'value'} );
+app.use('/product',(req,res,next)=>{
+    console.log(req.body);
+    res.redirect('/');
+   
+})
+app.use('/',(req,res,next)=>{
+    res.send('<h1>Hello from node Express</h1>');
+  
 })
 app.listen(3000);
