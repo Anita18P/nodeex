e4d518d6befe663cb9ef6c1fc53f3d284dda4a03
Anita18P <160405870+Anita18P@users.noreply.github.com>
2024-03-21 16:48:11 +0530

    retrieving data from database delete functionality

--- admin.js
+++ admin.js
@@ -15,8 +15,10 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(null,title, imageUrl, description, price);
-  product.save();
-  res.redirect('/');
+  product.save().then(()=>{
+    res.redirect('/');
+  }).catch(err=>console.log(err));
+  
 };
 
 exports.getEditProduct = (req, res, next) => {
@@ -52,20 +54,20 @@ exports.postEditProduct=(req,res,next)=>{
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll().then(([products]) => {
     res.render('admin/products', {
       prods: products,
       pageTitle: 'Admin Products',
       path: '/admin/products'
-    });
-  });
+    })
+  }).catch(err=>console.log(err));;
 };
  exports.postDeleteProduct=(req,res,next)=>{
   const prodId=req.body.productId;
-  console.log('in a post delete method foll is id')
-  console.log(prodId);
-  Product.deleteById(prodId);
-  res.redirect('/admin/products');
+  Product.deleteById(prodId).then(()=>{
+    res.redirect('/admin/products');
+  }).catch(err=>console.log(err));
+
   
    
 
