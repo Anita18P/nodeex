ac92e5b6edc52cd8ed5a5e9fbeea25a9ccda735c
Anita18P <160405870+Anita18P@users.noreply.github.com>
2024-03-26 12:53:25 +0530

    Blog post creation project

--- adminc.js
+++ adminc.js
@@ -1,62 +1,16 @@
-const expenseApp = require("../models/expenseDetails");
+const blogContent = require("../models/blogDetails")
 
-exports.getExpenseDetails= async(req,res,next)=>{
-  try {const expenses= await expenseApp.findAll()
-     res.status(200).json({allExpense:expenses});
-  }catch(err){console.log(err)};
-}
-
-exports.postExpenseDetails=async (req,res,next)=>{
-    console.log('hello');
-   
-     console.log('in postexpense method')
-     console.log(req.body);
-    try{  const expense=req.body.amount;
-        console.log('check it');
-        console.log(expense);
-    const description=req.body.description;
-    const expensecat=req.body.expensecat;
-    const data= await expenseApp.create({
-        Amount:expense,
-        Description:description,
-        ExpenseCategory:expensecat
-    });
-    console.log(data);
-    res.status(201).json({newExpenseDetails:data});
-}
-catch(err){
-    console.log('expense posting error');
-    console.log(err);
-}
-
-};
-exports.deleteExpenseDetails=async (req,res,next)=>{
-    const id=req.params.id;
-    await expenseApp.destroy({
-        where:{
-            id:id
-        }
-    }); 
-    res.sendStatus(200);
-    
-}
-exports.editExpenseDetails=async (req,res,next)=>{
-   try{ const expense=req.body.amount;
-    const description=req.body.description;
-    const expensecat=req.body.expensecat;
-    const uid=req.body.id;
-     const updata=await expenseApp.update({Amount:expense, Description:description,
-        ExpenseCategory:expensecat
-    },{where:{
-            id:uid
-        }
-    })
-    console.log('updated data by update method');
-    console.log(updata);
-    res.status(200).json({updated:updata});
+exports.postBlogDetails=async(req,res,next)=>{
+   try{ const title=req.body.title;
+    const author=req.body.author;
+    const content=req.body.content;
+ const data = await blogContent.create({
+    Title:title,
+    Author:author,
+    Content:content
+});
+   res.status(201).json({newBlogData:data});
 }catch(err){
-    console.log('error in editing');
     console.log(err);
 }
-
-}
+};
--- app.js
+++ app.js
@@ -1,54 +1,27 @@
-const path = require('path');
-
-const express = require('express');
-const bodyParser = require('body-parser');
+const express=require('express');
+const bodyParser=require('body-parser');
 const sequelize=require('./util/database');
-const errorController = require('./controllers/error');
-const Product=require('./models/product');
-const User=require('./models/user');
-
-const app = express();
-
+const path=require('path');
+const cors=require('cors');
+const app=express();
+app.use(cors());
 app.set('view engine', 'ejs');
-app.set('views', 'views');
+app.set('views', './views');
 
 const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
-
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use((req,res,next)=>{
-    User.findByPk(1).then(user=>{
-        req.user=user;
-        next();
-    })
-    .catch(err=>console.log(err));
-})
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
-
-app.use(errorController.get404);
-
-Product.belongsTo(User,{constrains:true,onDelete:"CASCADE"});
-User.hasMany(Product);
-
-
-sequelize
-// .sync({force:true})
-.sync()
-.then((result)=>{
-   return User.findByPk(1);
-    // console.log(result);
-}).then(user=>{
-    if(!user){
-      return  User.create({name:"Anita",email:"anita@gmail.com"})
-    }
-    //return Promise.resolve(user);
-    return user;
-}).then(user=>{
-    console.log(user);
-    app.listen(3000);
+app.use(bodyParser.json({ extended: false }));
+app.use(express.static(path.join(__dirname,'public')));
+   console.log('before route');
+ app.use('/admin',adminRoutes);
+ console.log('after route');
+sequelize.sync().then(result=>{
+    console.log(result);
+console.log('sequel response');
+    app.listen(4000);
+    console.log('after listeninng');
 
 })
-.catch(err=>console.log(err));
+.catch(err=>{
+    console.log(err);
+    console.log('sequel error');
+});
