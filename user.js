0133f07f6dce55890e09eb8709606fbcec593354
Anita18P <160405870+Anita18P@users.noreply.github.com>
2024-04-13 19:12:11 +0530

    expense tracker:6 blowfish encrypt
    Your user's password is at risk!!!!

--- user.js
+++ user.js
@@ -1,6 +1,7 @@
 const User=require('../models/User');
 const sequelize = require('../util/database');
 const {Sequelize,Op}=require('sequelize');
+const bcrypt=require('bcryptjs');
  
 exports.postUserDetails=async (req,res,next)=>{
     console.log("in postUserDetails");
@@ -12,15 +13,24 @@ exports.postUserDetails=async (req,res,next)=>{
   
     const Password=req.body.Password;
     console.log(Password);
-    const data=await User.create({
-        Name:Name,
-        Email:Email,
-         Password:Password,
+    const saltRound=10;
+    bcrypt.hash(Password,saltRound,async(err,hash)=>{
+        if(!err){
+            const data=await User.create({
+                Name:Name,
+                Email:Email,
+                 Password:hash,
+            })
+        }
+        else{
+            throw new Error("somthing went wrong");
+        }
     })
-       res.status(200).json({userData:data});
+   
+       res.status(200).json({message:"user created successfully"});
 }catch(error){
     console.log(error);
-res.status(403).json(error);
+res.status(403).json({message:error,success:false});
 }
     
 };
@@ -29,36 +39,60 @@ exports.userLogin=async (req,res,next)=>{
    try{ 
     const Email=req.body.Email;
     const Password=req.body.Password;
-    const user=await User.findOne({
+    const user=await User.findAll({
         where:{
             Email:Email
         }
         
         
     })
-    if(!user){
-        res.status(404).json({message:"user does not exist ",success:false});
+    console.log(user);
+    console.log(user.length);
+    if(user.length>0){
+        bcrypt.compare(Password,user[0].Password,(err,result)=>{
+           console.log("result and error");
+            console.log(result);
+           console.log(err);
+            if(err){
+            console.log("while decrypting");
+           throw new Error("Something went wrong");
+        }
+        if(result===true){
+            console.log("result");
+           return res.status(201).json({message:"user Logged in Successfully",success:true});
+        }
+        else{
+           return res.status(401).json({message:"user Not Authorised",success:false});
+
+        }
+       })
+    }else{
+       return res.status(404).json({message:"user does not exist ",success:false});
     }
-     const userPass=await User.findOne({
-        where:{
-            Email:Email,
-            where:Sequelize.where(Sequelize.fn('BINARY', Sequelize.col('Password')),
-            {
-                [Op.eq]:Password,
-            })
+    
+
+
+    //  const userPass=await User.findOne({
+    //     where:{
+    //         Email:Email,
+    //         where:Sequelize.where(Sequelize.fn('BINARY', Sequelize.col('Password')),
+    //         {
+    //             [Op.eq]:Password,
+    //         })
                 
             
-        }
+    //     }
 
-     })
-     if(!userPass){
-        res.status(401).json({message:"user Not Authorised",success:false});
-     }
+    //  })
+    //  if(!userPass){
+    //     res.status(401).json({message:"user Not Authorised",success:false});
+    //  }
 
-    res.status(201).json({message:"user Logged in Successfully",success:true});
+    // res.status(201).json({message:"user Logged in Successfully",success:true});
 
 }catch(error){
     console.log(error);
+    res.status(500).json({message:error,success:false});
     
 }
-}
+};
