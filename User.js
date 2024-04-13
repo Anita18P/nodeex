ca39092a3c4008c9565f55af94a9cdfdcf5e9c36
Anita18P <160405870+Anita18P@users.noreply.github.com>
2024-04-13 20:21:18 +0530

    expense tracker:7 Daily Expense App features

--- User.js
+++ User.js
@@ -2,9 +2,13 @@ const express = require('express');
 const attendance= require('../models/User');
 const router=express.Router();
 const admincontrollers=require('../controllers/user');
-
+const expenseControllers=require('../controllers/expense');
+router.get('/expense/get-expense',expenseControllers.getExpenses);
+router.post('/expense/add-expense',expenseControllers.addExpenses);
+router.post('/user-login',admincontrollers.userLogin);
  router.post('/sign-up',admincontrollers.postUserDetails);
- router.post('/user-login',admincontrollers.userLogin);
+ router.delete('/expense/delete-expenses/:id',expenseControllers.deleteExpense);
+
 
 
  module.exports = router;
